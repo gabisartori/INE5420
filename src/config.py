fb24e1395fe65265a103fde5149ae1059cab1a84
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-10-01 18:16:33 -0300

    Move config.py to src/

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-from numpy import ndarray
+import numpy as np
 
 HEIGHT: int = 900
 WIDTH: int = 1400
 ZOOM: float = 1.0
 
-WINDOW_NORMAL: ndarray = ndarray([0, 0, -1])
-WINDOW_POSITION: ndarray = ndarray([0, 0, 100])
+WINDOW_NORMAL: np.ndarray = np.array([0, 0, -1])
+WINDOW_POSITION: np.ndarray = np.array([0, 0, 100])
 
 APPLICATION_NAME: str = "INE5420 - SGI"
 INPUT_FILE: str | None = "example.obj"
